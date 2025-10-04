a9b96b481525023b04f8a0c94227a5073831687d
cheeseonamonkey <johndoe@example.com>
2025-10-03 21:01:01 -0600

    f

--- src/Layers/2_AnalysisLayer/1_Preprocessing/PositionMetrics.js
+++ src/Layers/2_AnalysisLayer/1_Preprocessing/PositionMetrics.js
@@ -1,21 +1,8 @@
 // PositionMetrics.js
 // Single position analysis utilities
 
-const { evaluateAggregate } = require('../EvaluationEngine');
 const Chess = require("chess.js").Chess;
 
-
-const evaluatePosition = (fen) => {
-  return evaluateAggregate(fen);
-};
-
-const calculateCentipawnLoss = (prevScore, currScore, isWhiteMove) => {
-  if (isWhiteMove) {
-    return Math.max(0, prevScore - currScore);
-  } else {
-    return Math.max(0, currScore - prevScore);
-  }
-};
 
 module.exports = {
   evaluatePosition,
