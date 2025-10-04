ed475cd8b91202ebff1b4164b5ea46288c34d03c
cheeseonamonkey <johndoe@example.com>
2025-10-03 18:10:17 -0600

    r

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const { Pipeline } = require("./Util/Pipeline");
 (async () => {
     console.log('Starting pipeline...');
 
-    const usernames = ['ffffattyyyy', 'ffatty100', 'ffatty110', 'ffatty190', 'ffatty120', 'ffatty', 'ffatty140'];
+    const usernames = ['ffffattyyyy', 'ffatty100', 'ffatty190', 'ffatty120', 'ffatty', 'ffatty140'];
 
     const mainPipeline = new Pipeline('Root', [
         async () => {
